80319a315c4d75ad61b30bd41ac19767ba9e4730
s7thiago <thyagosousasilva@gmail.com>
2021-02-06 17:50:43 -0300

    UI: Making responsive for mobile and tablet

--- lib/screens/main/components/list_of_emails.dart
+++ lib/screens/main/components/list_of_emails.dart
@@ -1,75 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:outlook/components/side_menu.dart';
 import 'package:outlook/models/Email.dart';
+import 'package:outlook/screens/email/email_screen.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 import '../../../constants.dart';
 import 'email_card.dart';
+import '../../../responsive.dart';
 
-class ListOfEmails extends StatelessWidget {
+class ListOfEmails extends StatefulWidget {
   const ListOfEmails({
     Key key,
   }) : super(key: key);
 
   @override
+  _ListOfEmailsState createState() => _ListOfEmailsState();
+}
+
+class _ListOfEmailsState extends State<ListOfEmails> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 250),
+        child: SideMenu(),
+      ),
       body: Container(
-        padding: EdgeInsets.all(kDefaultPadding),
         color: kBgDarkColor,
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (value) {},
-              decoration: InputDecoration(
-                hintText: "Search",
-                fillColor: kBgLightColor,
-                filled: true,
-                suffixIcon: WebsafeSvg.asset(
-                  "assets/Icons/Search.svg",
-                  width: 24,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide.none,
+        child: SafeArea(
+          right: false,
+          child: Column(
+            children: [
+              SizedBox(height: kDefaultPadding),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Row(
+                  children: [
+                    if (!Responsive.isDesktop(context))
+                      IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            _scaffoldKey.currentState.openDrawer();
+                          }),
+                    if (!Responsive.isDesktop(context)) SizedBox(width: 5),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          fillColor: kBgLightColor,
+                          filled: true,
+                          suffixIcon: Padding(
+                            padding:
+                                const EdgeInsets.all(kDefaultPadding * .75),
+                            child: WebsafeSvg.asset(
+                              "assets/Icons/Search.svg",
+                              width: 24,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: kDefaultPadding),
-            Row(
-              children: [
-                WebsafeSvg.asset(
-                  "assets/Icons/Angle down.svg",
-                  width: 16,
-                  color: Colors.black,
+              SizedBox(height: kDefaultPadding),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kDefaultPadding,
                 ),
-                SizedBox(width: 5),
-                Text(
-                  "Sort by date",
-                  style: TextStyle(fontWeight: FontWeight.w500),
+                child: Row(
+                  children: [
+                    WebsafeSvg.asset(
+                      "assets/Icons/Angle down.svg",
+                      width: 16,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "Sort by date",
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Spacer(),
+                    MaterialButton(
+                      minWidth: 20,
+                      onPressed: () {},
+                      child: WebsafeSvg.asset(
+                        "assets/Icons/Sort.svg",
+                        width: 16,
+                      ),
+                    ),
+                  ],
                 ),
-                Spacer(),
-                MaterialButton(
-                  minWidth: 20,
-                  onPressed: () {},
-                  child: WebsafeSvg.asset(
-                    "assets/Icons/Sort.svg",
-                    width: 16,
+              ),
+              SizedBox(height: kDefaultPadding),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: emails.length,
+                  itemBuilder: (context, index) => EmailCard(
+                    isActive:
+                        !Responsive.isDesktop(context) ? false : index == 0,
+                    email: emails[index],
+                    press: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EmailScreen(
+                                    email: emails[index],
+                                  )));
+                    },
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: kDefaultPadding),
-            Expanded(
-              child: ListView.builder(
-                itemCount: emails.length,
-                itemBuilder: (context, index) => EmailCard(
-                  isActive: index == 0,
-                  email: emails[index],
-                  press: () {},
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -2,26 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:outlook/components/side_menu.dart';
 import 'package:outlook/screens/email/email_screen.dart';
 import 'components/list_of_emails.dart';
+import '../../responsive.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Provê acesso a largura e altura da tela
+    Size _size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: SideMenu(),
-          ),
-          Expanded(
-            flex: 3,
-            child: ListOfEmails(),
-          ),
-          Expanded(
-            flex: 8,
-            child: EmailScreen(),
-          ),
-        ],
+      // Sempre que a largura da tela é menor do que 1340, sem responsividade temos um overflow
+
+      body: Responsive(
+        mobile: ListOfEmails(),
+        tablet: Row(
+          children: [
+            Expanded(flex: 6, child: ListOfEmails()),
+            Expanded(flex: 9, child: EmailScreen())
+          ],
+        ),
+        desktop: Row(
+          children: [
+            Expanded(
+              flex: _size.width > 1340 ? 2 : 4,
+              child: SideMenu(),
+            ),
+            Expanded(
+              flex: _size.width > 1340 ? 3 : 5,
+              child: ListOfEmails(),
+            ),
+            Expanded(
+              flex: _size.width > 1340 ? 8 : 10,
+              child: EmailScreen(),
+            ),
+          ],
+        ),
       ),
     );
   }
